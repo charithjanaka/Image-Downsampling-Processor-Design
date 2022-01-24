9689ae2f854b2f5cb151f378821a2fb1d13a6415
Charith <charithjanaka987@gmail.com>
2022-01-24 23:00:57 +0530

    Edited dmar and dmdr modules

--- Verilog/src/dmar.v
+++ Verilog/src/dmar.v
@@ -1,36 +1,24 @@
-// Module: Program Counter
-// Author: M.M.C.J.Bandara - 180065C
-// 06/01/2022
+// Module: Data Memory Address Redister (DMAR)
 
-module dmar(
-    input clk,                      // Clock
-    input  A_EN [3:0],              // Enable to A bus
-    input  B_EN [3:0],              // Enable to B bus
-    input  C_EN [3:0],              // Enable to be written from C bus
-    input  RST  [3:0],              // Reset
-    input  c_in [18:0],             // Data to be written from C bus
-    output a_out [18:0],            // Output data to A bus
-    output b_out [18:0],            // Output data to B bus
-    output addr_out [18:0]          // Address output to data memory
-);
+module dmar( clk, RST, DATA_EN, data_in, data_out);
 
-reg [18:0] addr;
-initial addr = 19'b0000000000000000000;
+input wire clk;                      // Clock
+input wire RST  [3:0];               // Reset
+input wire DATA_EN [3:0];            // Data Enable 
+input wire data_in [18:0];           // Data to be written
+
+output reg [18:0] data_out = 19'b0;  // Data output 
 
 always @ (posedge clk)
 begin
-    if (RST == 1)
-        addr <= 19'b0000000000000000000;
+    if (RST == 4'b0001)
+        data_out <= 19'b0;
     else
     begin
-        if (A_EN == 4'b0001)
-            a_out <= addr;
-        else if (B_EN == 4'b0001)
-            b_out <= addr;
-        else if (C_EN == 4'b0001)
-            addr <= c_in;
+        if (DATA_EN == 4'b0001)
+            data_out <= data_in;
         else
-            addr_out <= addr;
+            data_out <= data_out;
     end
 end
 
--- Verilog/src/dmdr.v
+++ Verilog/src/dmdr.v
@@ -1,40 +1,29 @@
-// Module: Program Counter
-// Author: M.M.C.J.Bandara - 180065C
-// 06/01/2022
+// Module: Data Memory Data Register (DMDR)
 
-module dmdr(
-    input clk,                      // Clock
-    input  A_EN [3:0],              // Enable to A bus
-    input  B_EN [3:0],              // Enable to B bus
-    input  C_EN [3:0],              // Enable to be written from C bus
-    input  RST  [3:0],              // Reset
-    input  c_in [18:0],             // Data to be written from C bus
-    output mem_in [7:0]             // Data from data memory
-    output a_out [18:0],            // Output data to A bus
-    output b_out [18:0],            // Output data to B bus
-    output mem_out [7:0]            // Output port to data memory
-);
+module dmdr( clk, RST, DATA_EN, MEM_EN, data_in, mem_in, data_out);
 
-reg [18:0] data;
-initial data = 19'b0000000000000000000;
+input wire clk;                      // Clock
+input wire RST [3:0];                // Reset
+input wire DATA_EN [3:0];            // Data Enable 
+input wire MEM_EN;                   // Memory Data Enable
+input wire data_in [18:0];           // Data to be written
+input wire mem_in [7:0];             // Data from Memory
+
+output reg [18:0] data_out = 19'b0;  // Data output
 
 always @ (posedge clk)
 begin
-    if (RST == 1)
-        data <= 19'b0000000000000000000;
+    if (RST == 4'b0010)
+        data <= 19'b0;
     else
     begin
-        if (A_EN == 4'b0010)
-            a_out <= data;
-        else if (B_EN == 4'b0010)
-            b_out <= data;
-        else if (C_EN == 4'b0010)
-            data <= c_in;
+        if (DATA_EN == 4'b0010)
+            data_out <= data_in;
+        else if (MEM_EN == 1)
+            data_out <= {{11{0}},mem_in[7:0]};
         else
-            data <= {{11{0}},mem_in[7:0]};
+            data_out <= data_out;
     end
 end
-
-assign mem_out = data[7:0];
 
 endmodule
